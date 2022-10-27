023a6bbc9a5de627f541128c22ef52a4a319bc2a
ritaaktay <ritaaktay@gmail.com>
2022-10-27 16:32:39 +0100

    green test for second move backwards

--- src/mars_rover.ts
+++ src/mars_rover.ts
@@ -1,5 +1,35 @@
-export const rover = {
-  location: "",
-  direction: "",
-  grid: ""
+export class MarsRover {
+  public location: {
+    x: number,
+    y: number
+  };
+  public direction: "e" | "w" | "s" | "n";
+  public grid: [number, number];
+
+  constructor(location: [number, number], direction: "e" | "w" | "s" | "n", grid: [number, number]) {
+    this.location = {
+      x: location[0],
+      y: location[1]
+    };
+    this.direction = direction;
+    this.grid = grid;
+  }
+
+  move = (commands: string): void => {
+    if (commands == "f") this.location.x += 1
+    if (commands == "b") {
+      if(this.location.x == 1) {
+        this.location.x = 0
+      }else if (this.location.x == 2) {
+        this.location.x = 1
+      }
+    }
+    if (commands == "l") this.direction = "n"
+    if (commands == "r") this.direction = "s"
+  }
+
+  getLocation = (): [number, number] => {
+    return [this.location.x, this.location.y]
+  }
 }
+
--- tests/mars_rover.test.ts
+++ tests/mars_rover.test.ts
@@ -1,15 +1,55 @@
-import { rover } from "../src/mars_rover"
+import { MarsRover } from "../src/mars_rover"
 
 describe("Mars Rover", () => {
   it("has a location", () => {
-    expect(rover.location).not.toBeUndefined()
+    const rover = new MarsRover([0,0], "e", [50,50])
+    expect(rover.getLocation()).toEqual([0,0])
   })
 
   it("has a direction", () => {
-    expect(rover.direction).not.toBeUndefined()
+    const rover = new MarsRover([0,0], "e", [50,50])
+    expect(rover.direction).toEqual("e")
   })
 
   it("has a grid", () => {
-    expect(rover.grid).not.toBeUndefined()
+    const rover = new MarsRover([0,0], "e", [50,50])
+    expect(rover.grid).toEqual([50,50])
+  })
+
+  it("con move forward", () => {
+    const rover = new MarsRover([0,0], "e", [50,50])
+    rover.move("f")
+    expect(rover.getLocation()).toEqual([1,0])
+  })
+
+  it("can move backward", () => {
+    const rover = new MarsRover([1,0], "e", [50,50])
+    rover.move("b")
+    expect(rover.getLocation()).toEqual([0,0])
+  })
+
+  it("can turn left", () => {
+    const rover = new MarsRover([1,0], "e", [50,50])
+    rover.move("l")
+    expect(rover.direction).toEqual("n")
+  })
+
+  it("can turn right", () => {
+    const rover = new MarsRover([1,0], "e", [50,50])
+    rover.move("r")
+    expect(rover.direction).toEqual("s")
+  })
+
+  it("con move forward", () => {
+    const rover = new MarsRover([1,0], "e", [50,50])
+    rover.move("f")
+    expect(rover.getLocation()).toEqual([2,0])
+  })
+
+
+  it("can move backward", () => {
+    const rover = new MarsRover([2,0], "e", [50,50])
+    rover.move("b")
+    expect(rover.getLocation()).toEqual([1,0])
   })
 })
